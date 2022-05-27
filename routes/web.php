ee5eaa8252bd899a6e21190a79b8b25ca4701ae8
godwin <godwin@fincra.com>
2022-05-27 07:56:15 +0100

    removing of default page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(["message" => "health check all good"]);
 });
 
 
