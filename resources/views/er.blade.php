bed9f535b0e80a0e1bdc751c8d655babb0cc79c9
godwin <godwin@fincra.com>
2022-05-28 17:43:20 +0100

    impelemntation of work flow

---
+++ resources/views/er.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hostel World</title>
+    <!-- Fonts -->
+    <link href="/assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="antialiased">
+    <div class="er-image-wrap">
+        <div class="er-image">
+            <div class="er-text">
+                <h2>Assessment Work Flow</h2>
+            </div>
+            <img src="/assets/images/er.png" alt="er" />
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/health', function () {
     return response()->json(["message" => "health check all good"]);
 });
+Route::get('/er', function () {
+    return view('er');
+});
 
 
 Route::fallback(function (Request $request) {
